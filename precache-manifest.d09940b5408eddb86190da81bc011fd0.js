128319ff328896ff8175f3f14f46fd38b8775e0a
Arzakan <arzakanporchkhidze17@mgail.com>
2020-09-14 22:44:49 +0400

    Updates

---
+++ precache-manifest.d09940b5408eddb86190da81bc011fd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd3b2fe437bd074cc7ffa0c880c1818",
+    "url": "/PomodoroClock/index.html"
+  },
+  {
+    "revision": "e9f70c1a30aa9be6df8a",
+    "url": "/PomodoroClock/static/css/main.916ac321.chunk.css"
+  },
+  {
+    "revision": "ba0568b412bff569c888",
+    "url": "/PomodoroClock/static/js/2.1aa08753.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PomodoroClock/static/js/2.1aa08753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f70c1a30aa9be6df8a",
+    "url": "/PomodoroClock/static/js/main.0df81b11.chunk.js"
+  },
+  {
+    "revision": "e1915c06ba7238173dc8",
+    "url": "/PomodoroClock/static/js/runtime-main.f180ccf7.js"
+  }
+]);
